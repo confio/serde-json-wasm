7474d96e3de34d8c7d57e3f0dc2fee7b089d2d4f
CyberHoward <CyberHoward@protonmail.com>
2022-11-21 14:50:44 +0000

    Implement MapKeySerializer

--- src/ser/mod.rs
+++ src/ser/mod.rs
@@ -7,6 +7,8 @@ use serde::ser;
 use serde::ser::SerializeStruct as _;
 use serde::Serialize;
 
+use std::vec::Vec;
+
 use self::map::SerializeMap;
 use self::seq::SerializeSeq;
 use self::struct_::{SerializeStruct, SerializeStructVariant};
@@ -519,18 +521,188 @@ impl ser::Error for Error {
     }
 }
 
-/// An unreachable type to fill the SerializeTupleVariant type
-pub enum Unreachable {}
+/// Wrapper around Serializer that only allows serialization of valid JSON key types (strings).
+struct MapKeySerializer<'a> {
+    ser: &'a mut Serializer,
+}
 
-impl ser::SerializeTupleVariant for Unreachable {
+fn key_must_be_a_string() -> Error {
+    Error::Custom("JSON object key is required to be a string type.".to_string())
+}
+
+impl<'a> ser::Serializer for MapKeySerializer<'a> {
     type Ok = ();
     type Error = Error;
+    type SerializeSeq = SerializeSeq<'a>;
+    type SerializeTuple = SerializeSeq<'a>;
+    type SerializeTupleStruct = SerializeSeq<'a>;
+    type SerializeTupleVariant = SerializeSeq<'a>;
+    type SerializeMap = SerializeMap<'a>;
+    type SerializeStruct = SerializeStruct<'a>;
+    type SerializeStructVariant = SerializeStructVariant<'a>;
 
-    fn serialize_field<T: ?Sized>(&mut self, _value: &T) -> Result<()> {
-        unreachable!()
+    fn serialize_bool(self, _value: bool) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+    #[inline]
+    fn serialize_str(self, value: &str) -> Result<()> {
+        self.ser.serialize_str(value)
     }
 
-    fn end(self) -> Result<Self::Ok> {
+    #[inline]
+    fn serialize_unit_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        variant: &'static str,
+    ) -> Result<()> {
+        self.ser.serialize_str(variant)
+    }
+
+    #[inline]
+    fn serialize_newtype_struct<T>(self, _name: &'static str, value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        value.serialize(self)
+    }
+
+    fn serialize_i8(self, _value: i8) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_i16(self, _value: i16) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_i32(self, _value: i32) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_i64(self, _value: i64) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_i128(self, _value: i128) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_u8(self, _value: u8) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_u16(self, _value: u16) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_u32(self, _value: u32) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_u64(self, _value: u64) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_u128(self, _value: u128) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_f32(self, _value: f32) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_f64(self, _value: f64) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_char(self, value: char) -> Result<()> {
+        self.ser.serialize_str(&value.to_string())
+    }
+
+    fn serialize_bytes(self, _value: &[u8]) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_unit(self) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_unit_struct(self, _name: &'static str) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_newtype_variant<T>(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _value: &T,
+    ) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_none(self) -> Result<()> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_some<T>(self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + Serialize,
+    {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_seq(self, _len: Option<usize>) -> Result<Self::SerializeSeq> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_tuple(self, _len: usize) -> Result<Self::SerializeTuple> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_tuple_struct(
+        self,
+        _name: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleStruct> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_tuple_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeTupleVariant> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_map(self, _len: Option<usize>) -> Result<Self::SerializeMap> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_struct(self, _name: &'static str, _len: usize) -> Result<Self::SerializeStruct> {
+        Err(key_must_be_a_string())
+    }
+
+    fn serialize_struct_variant(
+        self,
+        _name: &'static str,
+        _variant_index: u32,
+        _variant: &'static str,
+        _len: usize,
+    ) -> Result<Self::SerializeStructVariant> {
+        Err(key_must_be_a_string())
+    }
+
+    fn collect_str<T>(self, _value: &T) -> Result<()>
+    where
+        T: ?Sized + fmt::Display,
+    {
         unreachable!()
     }
 }
@@ -1175,6 +1347,25 @@ mod tests {
         map.insert("my_age", 28);
         assert_eq!(to_string(&map).unwrap(), r#"{"my_age":28}"#);
 
+        #[derive(Debug, Serialize, PartialEq, Eq, Hash)]
+        pub struct NewType(String);
+
+        // New type wrappers around String types work as keys
+        let mut map = HashMap::new();
+        map.insert(NewType(String::from("my_age")), 44);
+        assert_eq!(to_string(&map).unwrap(), r#"{"my_age":44}"#);
+
+        #[derive(Debug, Serialize, PartialEq, Eq, Hash)]
+        #[serde(rename_all = "lowercase")]
+        pub enum MyResult {
+            Err,
+        }
+
+        // Unit variants are also valid keys
+        let mut map = HashMap::new();
+        map.insert(MyResult::Err, 404);
+        assert_eq!(to_string(&map).unwrap(), r#"{"err":404}"#);
+
         // HashMap does not have deterministic iteration order (except in the Wasm target).
         // So the two element map is serialized as one of two options.
         let mut two_values = HashMap::new();
@@ -1184,6 +1375,127 @@ mod tests {
         assert!(
             serialized == r#"{"her_name":"aline","my_name":"joseph"}"#
                 || serialized == r#"{"my_name":"joseph","her_name":"aline"}"#
+        );
+    }
+
+    #[test]
+    fn invalid_json_key() {
+        use crate::ser::key_must_be_a_string;
+        use std::collections::HashMap;
+
+        // i8 key
+        let mut map = HashMap::new();
+        map.insert(1i8, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // i16 key
+        let mut map = HashMap::new();
+        map.insert(40i16, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // i32 key
+        let mut map = HashMap::new();
+        map.insert(40i32, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // i64 key
+        let mut map = HashMap::new();
+        map.insert(40i64, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // u8 key
+        let mut map = HashMap::new();
+        map.insert(1u8, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // u16 key
+        let mut map = HashMap::new();
+        map.insert(40u16, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // u32 key
+        let mut map = HashMap::new();
+        map.insert(40u32, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // u64 key
+        let mut map = HashMap::new();
+        map.insert(40u64, "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        #[derive(Debug, Serialize, PartialEq, Eq, Hash)]
+        #[serde(rename_all = "lowercase")]
+        pub enum MyResult {
+            Unit(()),
+            Ok(Response),
+        }
+        #[derive(Debug, Serialize, PartialEq, Eq, Hash)]
+        pub struct Response {
+            pub log: Option<String>,
+            pub count: i64,
+            pub list: Vec<u32>,
+        }
+
+        // unit enum
+        let mut map = HashMap::new();
+        map.insert(MyResult::Unit(()), "my_age");
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // struct enum
+        let mut map = HashMap::new();
+        map.insert(
+            MyResult::Ok(Response {
+                log: None,
+                count: 1,
+                list: vec![6],
+            }),
+            "my_age",
+        );
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
+        );
+
+        // Struct
+        let mut map = HashMap::new();
+        map.insert(
+            Response {
+                log: None,
+                count: 1,
+                list: vec![6],
+            },
+            "my_age",
+        );
+        assert_eq!(
+            to_string(&map).unwrap_err().to_string(),
+            key_must_be_a_string().to_string()
         );
     }
 
