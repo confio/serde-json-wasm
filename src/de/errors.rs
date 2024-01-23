249825a468aaf92b5271654c044e8536357292f3
Christoph Otter <chris@confio.gmbh>
2024-01-23 16:54:38 +0100

    Fix typo

--- src/de/errors.rs
+++ src/de/errors.rs
@@ -75,7 +75,7 @@ pub enum Error {
     /// JSON has a comma after the last value in an array or map.
     TrailingComma,
 
-    /// JSON is nested too deeply, exceeeded the recursion limit.
+    /// JSON is nested too deeply, exceeded the recursion limit.
     RecursionLimitExceeded,
 
     /// Custom error message from serde
@@ -131,7 +131,7 @@ impl core::fmt::Display for Error {
                      value."
                 }
                 Error::TrailingComma => "JSON has a comma after the last value in an array or map.",
-                Error::RecursionLimitExceeded => "JSON is nested too deeply, exceeeded the recursion limit.",
+                Error::RecursionLimitExceeded => "JSON is nested too deeply, exceeded the recursion limit.",
                 Error::Custom(msg) => msg,
             }
         )
